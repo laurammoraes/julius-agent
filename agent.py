0e44941c22d177818e39e8d3270821ea812a3336
laurammoraes2@gmail.com <laura@poontoagencia.com>
2025-02-23 14:35:22 -0300

    Feat: adicionar framework fastapi

--- agent.py
+++ agent.py
@@ -17,29 +17,28 @@ async  def makeAction(action:str):
     try:
        
 
-        config = BrowserConfig(
-            headless=False,
-            disable_security=True
+        
+        config = BrowserContextConfig(
+            cookies_file="path/to/cookies.json",
+            wait_for_network_idle_page_load_time=3.0,
+            browser_window_size={'width': 1280, 'height': 1100},
+            locale='en-US',
+            user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
+            highlight_elements=True,
+            viewport_expansion=500,
+            allowed_domains=['google.com', 'wikipedia.org'],
         )
-        # config = BrowserContextConfig(
-        #     cookies_file="path/to/cookies.json",
-        #     wait_for_network_idle_page_load_time=3.0,
-        #     browser_window_size={'width': 1280, 'height': 1100},
-        #     locale='en-US',
-        #     user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
-        #     highlight_elements=True,
-        #     viewport_expansion=500,
-        #     allowed_domains=['google.com', 'wikipedia.org'],
-        # )
 
         browser = Browser()
-        context = BrowserContext(browser=browser, config=config)
+        # context = BrowserContext(browser=browser, config=config)
         
         agent = Agent(
             task=action,
-            browser=context,
+            # browser=context,
             llm=llm,
-            # browser=browser,
+            browser=browser,
+            use_vision=True,             
+            save_conversation_path="logs/conversation"  
            
         )
 
@@ -55,4 +54,5 @@ async  def makeAction(action:str):
     except Exception as e:
         print(f"❌ Erro ocorrido: {e}")
 
-# asyncio.run(main())
+# asyncio.run(makeAction())
+
